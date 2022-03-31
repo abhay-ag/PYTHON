7f0816820e79ae214a327c9931e97f377047e8e4
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-31 14:06:34 +0530

    Login and Create Function Started

--- ATM Project/atm_project.py
+++ ATM Project/atm_project.py
@@ -1,10 +1,41 @@
+userArray = []
+passArray = []
+balance = [0] * 9999
 def start():
     print("Welcome to the ATM Project\n\n")
     choice = ''
     i = 0
     while choice != 'l' and choice != 'c' and choice != 'q':
-        choice = input("Choose from the options below\n\tl -> login\n\tc -> create an account \n\tq -> quit\n\n>")
+        choice = input("\nChoose from the options below\n\tl -> login\n\tc -> create an account \n\tq -> quit\n\n> ")
         if ( i== 3):
             exit(0)
         i += 1
         
+    if choice == "l":
+        login()
+    elif choice == "c":
+        createAccount()
+    elif choice == "q":
+        exit(0)
+
+def login():
+    global user, paswd
+    if (len(userArray) == 0 and len(passArray) == 0):
+        print("\nCurrently we have no accounts! Please register first!!")
+        start()
+    else:
+        user = input("\n\nEnter your username: ")
+        if(not(userArray.index(user))):
+            print("\nNo such user exists! Please register first!!")
+        else:
+            paswd = input("\nEnter your password: ")
+            if(paswd == passArray[userArray.index(user)]):
+                print("\n****\tLOGIN SUCCESSFUL\t****")
+            else:
+                print("\nWRONG PASSWORD! Please start again")
+
+
+def createAccount():
+    user = input("\nPlease enter your User Name: ")
+
+start()
